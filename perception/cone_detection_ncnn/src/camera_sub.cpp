c9c10c683029568b6e68a7ae5af4dbb634e150aa
Tocoquinho <giovannicmorales@gmail.com>
2022-10-29 20:53:21 -0300

    FIx camera sub bridge func

--- perception/cone_detection_ncnn/src/camera_sub.cpp
+++ perception/cone_detection_ncnn/src/camera_sub.cpp
@@ -16,12 +16,16 @@ class CameraSubscriber : public rclcpp::Node
     CameraSubscriber(): Node("camera_subscriber")
     {
       _subscription = this->create_subscription<sensor_msgs::msg::Image>("raw_image", 10, bind(&CameraSubscriber::image_callback, this, _1));
+      namedWindow("Webcam");
     }
 
   private:
     void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
     {
-      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+      imshow("Webcam", cv_ptr->image);
+      waitKey(1);
     }
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _subscription;
 };
