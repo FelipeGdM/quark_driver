887ad613d58a0ecd05661b4455d8d4d7108bfa54
Felipe Gomes de Melo <felipegmelo@usp.br>
2023-05-16 17:53:18 -0300

    Update used YOLO weights

--- perception/cone_detection_python/cone_detection_python/camera_sub_node.py
+++ perception/cone_detection_python/cone_detection_python/camera_sub_node.py
@@ -21,7 +21,7 @@ class ImageSubscriber(Node):
         self.cone_centers_pub = self.create_publisher(PoseArray, '/camera/cone_centers', 1)
         
         #self.yolov7 = Yolov7("/home/tocoquinho/repositories/ros2_ws/src/quark_driver/perception/cone_detection_python/params/tiny_cone_weights.pt", 0.4, 0.25, 480, None)
-        self.yolov7 = Yolov7("/home/quark/Documents/yolov7/230_epochs_tiny.pt", 0.6, 0.5, 1280, None)
+        self.yolov7 = Yolov7("/home/quark/Documents/yolov7/400_epochs_tiny.pt", 0.6, 0.5, 1280, None)
         print("Ready to receive")
 
     def image_callback(self, msg):
@@ -40,8 +40,8 @@ class ImageSubscriber(Node):
                 rect_center.position.y = float(rects[3] + rects[1])/2
                 centers.poses.append(rect_center)
 
-                if len(centers.poses) >= 4:
-                    break
+                # if len(centers.poses) >= 4:
+                #     break
 
         self.detections_pub.publish(self.bridge.cv2_to_imgmsg(display_img, "bgr8"))
         self.cone_centers_pub.publish(centers)
